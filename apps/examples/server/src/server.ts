85757915113582c272a7750d6c60bd9f41f1739e
Panhaboth Kun <panhabothk@outlook.com>
2025-08-15 11:42:38 +0000

    - Used env variables for API tokens - Configured nodemon watch

--- apps/examples/server/src/server.ts
+++ apps/examples/server/src/server.ts
@@ -11,7 +11,13 @@ app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
 
-app.post("/create-transaction", (req, res) => {
+const client = new ABAPayWayClient(
+  process.env.ABA_MERCHANT_ID,
+  process.env.ABA_API_KEY,
+  process.env.ABA_RSA_PUBLIC_KEY
+);
+
+app.get("/create-transaction", (req, res) => {
   // Here you would typically handle the transaction creation logic
   // For example, using the ABAPayWayClient to create a transaction
   //   const client = new ABAPayWayClient(
@@ -22,15 +28,18 @@ app.post("/create-transaction", (req, res) => {
   // );
   // client.createPaymentLink("Test", 2000, "KHR", "https://example.com/return");
 
-  res.send(`
-    <form action="..." method="POST" target="aba_webservice>
-      <input type="hidden" name="request_time" value="...">
-      <input type="hidden" name="merchant_id" value="...">
-      <input type="hidden" name="merchant_auth" value="...">
-      <input type="hidden" name="hash" value="...">
-      <button type="submit">Pay Now</button>
-    </form>
-    <iframe name="aba_webservice" style="display:none;"></iframe>
-    ...
-    `);
+  const htmlString = client.createTransaction({
+    tran_id: "tran1234",
+    amount: "20",
+    payer: {
+      firstname: "John",
+      lastname: "Doe",
+      phone: "0123456789",
+      email: "panhabothk@outlook.com",
+    },
+    return_params: "param1=value1&param2=value2",
+    payment_option: "cards",
+  });
+
+  res.send(htmlString); // Send the HTML string as the response
 });
